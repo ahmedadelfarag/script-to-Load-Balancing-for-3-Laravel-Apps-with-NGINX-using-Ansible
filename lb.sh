dba8f0e3ac11d275d8c832899b267dd9304c24f8
ahmedadelfarag <a.adel@cloud-11.com>
2022-02-13 16:52:58 +0200

    v1

--- lb.sh
+++ lb.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+#Apps IPs
+server1=10.91.141.29
+server2=10.91.141.26
+server3=10.91.141.70
+
+#app name
+
+appname=adel
+
 yum update -y
 yum -y install yum-utils
 
-basearch=$(rpm -q --qf "%{arch}" -f /etc/$distro)
-
-#Apps IPs
-server1=10.0.0.0
-server2=10.0.0.0
-server3=10.0.0.0
-
-cat <<EOF | tee /etc/yum.repos.d/nginx.repo
+cat <<'EOF' | tee /etc/yum.repos.d/nginx.repo
 [nginx-stable]
 name=nginx stable repo
 baseurl=http://nginx.org/packages/centos/7/$basearch/
@@ -33,12 +35,9 @@ systemctl start nginx
 
 systemctl enable nginx
 
-
 rm /etc/nginx/conf.d/default.conf
 
-touch /etc/nginx/conf.d/default.conf
-
-
+cat <<'EOF' | tee /etc/nginx/conf.d/default.conf
 upstream adel {
     server $server1:9000;
     server $server2:9000;
@@ -48,7 +47,7 @@ server {
     listen       80;
     server_name  localhost default_server;
     
-    root /var/www/adel/public;
+    root /var/www/$appname/public;
     add_header X-Frame-Options "SAMEORIGIN";
     add_header X-Content-Type-Options "nosniff";
     index index.php index.html index.htm;
@@ -63,9 +62,9 @@ server {
     # pass the PHP scripts to FastCGI server listening on IP:9000
     location ~ \.php$ {
         fastcgi_split_path_info ^(.+\.php)(.*)$;
-        fastcgi_pass   adel;
+        fastcgi_pass   $appname;
         fastcgi_index  index.php;
-        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
+        fastcgi_param SCRIPT_FILENAME "$realpath_root$fastcgi_script_name";
         include        fastcgi_params;
     }
     
@@ -73,8 +72,14 @@ server {
         deny all;
     }
 }
+EOF
 
-useradd -r -M -U www-data
-mkdir -p /var/www/adel
-chown -R www-data:www-data /var/www/adel
+sed -i "s/\$server1/$server1/g" /etc/nginx/conf.d/default.conf
+sed -i "s/\$server2/$server2/g" /etc/nginx/conf.d/default.conf
+sed -i "s/\$server3/$server3/g" /etc/nginx/conf.d/default.conf
+
+sed -i "s/\$appname/$appname/g" /etc/nginx/conf.d/default.conf
+
+mkdir -p /var/www/$appname
+chown -R nginx:nginx /var/www/$appname
 systemctl reload nginx
